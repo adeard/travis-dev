b72abd598355a4fad2c7577e3863eade8dd662f8
adeard <adeardian1994@gmail.com>
2023-11-01 17:13:28 +0700

    add travis logo icon

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Col, Row, Statistic } from 'antd';
 import ContentLayout from '../components/Layouts/ContentLayout'
 
 export default function HomePage() {
     return (    
         <ContentLayout current_page="home">
-            Homepagea
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card bordered={false}>
+                        <Statistic
+                        title="Active"
+                        value={11.28}
+                        precision={2}
+                        valueStyle={{
+                            color: '#3f8600',
+                        }}
+                        prefix={<ArrowUpOutlined />}
+                        suffix="%"
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card bordered={false}>
+                        <Statistic
+                        title="Idle"
+                        value={9.3}
+                        precision={2}
+                        valueStyle={{
+                            color: '#cf1322',
+                        }}
+                        prefix={<ArrowDownOutlined />}
+                        suffix="%"
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card bordered={false} style={{ backgroundColor:"#a3a2a7" }}>
+                        <Statistic
+                        title="Idle"
+                        value={9.3}
+                        precision={2}
+                        valueStyle={{
+                            color: '#cf1322',
+                        }}
+                        prefix={<ArrowDownOutlined />}
+                        suffix="%"
+                        />
+                    </Card>
+                </Col>
+            </Row>
         </ContentLayout>
     )
 }
