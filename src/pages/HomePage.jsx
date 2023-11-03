af8e9285b33c4e935bb2f3e31cad1a8d45dea189
adeard <adeardian1994@gmail.com>
2023-11-03 17:08:05 +0700

    add statistic task

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { 
+    DatabaseFilled, 
+    SoundFilled, 
+    RocketFilled, 
+    HomeFilled, 
+    CheckCircleFilled 
+} from '@ant-design/icons';
 import { Card, Col, Row, Statistic } from 'antd';
 import ContentLayout from '../components/Layouts/ContentLayout'
 
@@ -8,44 +14,70 @@ export default function HomePage() {
         <ContentLayout current_page="home">
             <Row gutter={16}>
                 <Col span={8}>
-                    <Card bordered={false}>
-                        <Statistic
-                        title="Active"
-                        value={11.28}
-                        precision={2}
+                    <Card bordered={false} style={{ backgroundColor:"#4F6F52" }}>
+                        <Statistic 
+                        title={<p style={{margin:"0px", color:"white", fontSize:"14px"}}>Assigned</p>}
+                        value={9}
                         valueStyle={{
-                            color: '#3f8600',
+                            color: 'white',
                         }}
-                        prefix={<ArrowUpOutlined />}
-                        suffix="%"
+                        prefix={<DatabaseFilled />}
+                        suffix="Task"
                         />
                     </Card>
                 </Col>
                 <Col span={8}>
-                    <Card bordered={false}>
+                    <Card bordered={false} style={{ backgroundColor:"#4F6F52" }}>
                         <Statistic
-                        title="Idle"
-                        value={9.3}
-                        precision={2}
+                        title={<p style={{margin:"0px", color:"white", fontSize:"14px"}}>Notify Driver</p>}
+                        value={2}
                         valueStyle={{
-                            color: '#cf1322',
+                            color: 'white',
                         }}
-                        prefix={<ArrowDownOutlined />}
-                        suffix="%"
+                        prefix={<SoundFilled />}
+                        suffix="Task"
                         />
                     </Card>
                 </Col>
                 <Col span={8}>
-                    <Card bordered={false} style={{ backgroundColor:"#a3a2a7" }}>
+                    <Card bordered={false} style={{ backgroundColor:"#4F6F52" }}>
                         <Statistic
-                        title="Idle"
-                        value={9.3}
-                        precision={2}
+                        title={<p style={{margin:"0px", color:"white", fontSize:"14px"}}>Started</p>}
+                        value={23}
                         valueStyle={{
-                            color: '#cf1322',
+                            color: 'white',
                         }}
-                        prefix={<ArrowDownOutlined />}
-                        suffix="%"
+                        prefix={<RocketFilled />}
+                        suffix="Task"
+                        />
+                    </Card>
+                </Col>
+            </Row>
+            <br />
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Card bordered={false} style={{ backgroundColor:"#4F6F52" }}>
+                        <Statistic
+                        title={<p style={{margin:"0px", color:"white", fontSize:"14px"}}>Arrived</p>}
+                        value={14}
+                        valueStyle={{
+                            color: 'white',
+                        }}
+                        prefix={<HomeFilled />}
+                        suffix="Task"
+                        />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card bordered={false} style={{ backgroundColor:"#4F6F52" }}>
+                        <Statistic
+                        title={<p style={{margin:"0px", color:"white", fontSize:"14px"}}>Completed</p>}
+                        value={11}
+                        valueStyle={{
+                            color: 'white',
+                        }}
+                        prefix={<CheckCircleFilled />}
+                        suffix="Task"
                         />
                     </Card>
                 </Col>
